6883c09ca05097932cf802122783139112176a8d
anas-0921 <anas092121@gmail.com>
2024-08-20 00:42:49 +0530

    task 1 completed

---
+++ script.js
@@ -0,0 +1,7 @@
+let element = document.querySelector("#myid");
+console.log(element);
+
+window.addEventListener("scroll", () => {
+    // console.log("Success");
+    document.querySelector("nav").style.backgroundColor="lightseagreen";
+})
